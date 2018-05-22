d47e3a93158897bed6ed2280eaf5a6486b6670ba
hoodielive <hoodielive@gmail.com>
2018-05-22 07:43:42 -0400

    express lib http requests

--- myapp/app.js
+++ myapp/app.js
@@ -1,7 +1,21 @@
-const express = require('express');
+const express = require('express'); 
 
-const app = express();  
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello, Enochiana')); 
+app.get('/', (req, res) => { 
+	res.send('Hello Troy!') 
+}); 
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.post('/', (req, res) => {
+	res.send('Got a POST request') 
+}); 
+
+app.put('/user', (req, res) => {
+	res.send('Got a PUT request at /user')
+}); 
+
+app.delete('/user', (req, res) => {
+	res.send('Got a DELETE request at /user') 
+}); 
+
+app.listen(3000, () => console.log('Example app listening on port 3000')); 
